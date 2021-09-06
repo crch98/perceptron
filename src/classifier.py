9240cf5eafd6f0aad1de2a1a1f441d2d5be813ca
Carlos Ch <crch98@protonmail.com>
2021-09-06 01:11:30 -0500

    Implemented perceptron methods

--- src/classifier.py
+++ src/classifier.py
@@ -1,9 +1,25 @@
 class Perceptron:
 	def __init__(self, X, W):
-		pass
+		self.X = X
+		self.W = W	
 
-	def classify(self):
-		pass
+	def calculateV(self):
+		"""
+		Returns the multiplication of W matrix and X matrix
+		"""
+		return np.dot(self.W, self.X)
+
+	def activation(self):
+		"""
+		Implementation of the step function
+		"""
+		V = self.calculateV()
+		Y = np.zeros((V.shape[0],1))
+
+		for i in range(V.shape[0]):
+			Y[i] = 1 if V[i] >= 0 else 0
+
+		return Y
 
 	def getX(self):
 		pass
