fba02823a40ecfa4b07f56fa179a601ca28f0696
Carlos Ch <crch98@protonmail.com>
2021-09-06 21:27:32 -0500

    Added plot to gui

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 
+import matplotlib
+from matplotlib import pyplot as plt
+matplotlib.use("TkAgg")
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 root = Tk()
 
 mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
@@ -13,6 +19,20 @@ bias_lbl = ttk.Label(mainframe, text="Bias")
 bias = ttk.Entry(mainframe, width=6)
 
 submit = ttk.Button(mainframe, text="Clasificar")
+
+figure = Figure(figsize=(8, 8), dpi=100)
+plot = figure.add_subplot(1, 1, 1)
+plot.spines['left'].set_position('center')
+plot.spines['bottom'].set_position('center')
+
+plot.plot(0.5, 0.3, color="red", marker="o", linestyle="")
+
+x = [0.1, 0.2, 0.3]
+y = [-0.1, -0.2, -0.3]
+plot.plot(x, y, color="blue", marker="x", linestyle="")
+
+canvas = FigureCanvasTkAgg(figure, plotframe)
+canvas.get_tk_widget().grid(row=0, column=0)
 
 mainframe.grid(column=0, row=0, sticky=(N, S, E, W))
 w1_lbl.grid(column=0, row=0, sticky=E)
