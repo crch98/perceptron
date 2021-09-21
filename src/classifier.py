23b96184292547163f87e715be461be25be39472
Carlos Ch <crch98@protonmail.com>
2021-09-21 17:53:11 -0500

    Modified both classes to handle perceptron training

--- src/classifier.py
+++ src/classifier.py
@@ -1,25 +1,41 @@
 import numpy as np
 
 class Perceptron:
-	def __init__(self, X, W, bias):
-		self.X = X
-		self.W = W	
-		self.bias = bias
+    def __init__(self, W, expected, lr=0.1):
+        #self.X = X
+        #self.W = np.array([[bias], [W[0]], [W[1]]])
+        self.W = W
+        self.expected = expected
+        self.lr = lr
 
-	def calculateV(self):
-		"""
-		Returns the multiplication of W matrix and X matrix
-		"""
-		return np.dot(self.X, self.W) - self.bias
+    def calculateV(self, X):
+        """
+        Returns the multiplication of W matrix and X matrix
+        """
+        return np.dot(X, self.W[1:]) + self.W[0]
 
-	def activation(self):
-		"""
-		Implementation of the step function
-		"""
-		V = self.calculateV()
-		Y = np.zeros((V.shape[0],1))
+    def activation(self, X):
+        """
+        Implementation of the step function
+        """
+        return np.where(self.calculateV(X) >= 0.0, 1, -1)
 
-		for i in range(V.shape[0]):
-			Y[i] = 1 if V[i] >= 0 else 0
+    def train(self, X):
+        for xi, target in zip(X, self.expected):
+            update = self.lr * (target - self.activation(xi))
+            self.W[1:] += update * xi
+            self.W[0] += update
+            #print(self.W)
 
-		return Y
+
+# def main():
+#     X = np.array([[1, 1], [-0.5, 1], [3, 1], [-2, 1]])
+#     target = np.array([[1], [-1], [1], [-1]])
+#     #w = np.random.rand(1, 3)
+#     w = np.array((1, -2.5, 1.75))
+#     p = Perceptron(w, target, 0.5)
+
+#     p.train(X, target)
+
+
+# main()
--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 import numpy as np
+import time
 
 from classifier import Perceptron
 
@@ -15,10 +16,14 @@ class Gui:
     def __init__(self, root):
         root.title("Práctica Perceptrón")
         self.coord = []
+        self.coord_class = []
+        self.weights = np.random.rand(1, 3)
         
-        self.w1 = StringVar()        
+        self.w1 = StringVar()
         self.w2 = StringVar()
         self.bias = StringVar()
+        self.epoch = StringVar()
+        self.lr = StringVar()
         
         # create plot
         self.fig = Figure(figsize=(8, 8), dpi=100)
@@ -28,44 +33,56 @@ class Gui:
         self._create_widgets()
         
     def start(self):
-        w_1, w_2, bias = self._retrieve_values()       
-    
         X = np.asarray(self.coord)
-        W = [w_1, w_2]
-        p = Perceptron(X, W, bias)
-        Y = p.activation()
-    
-        self.ax.cla()       # clean the points on canvas
-        self._config_plot() # set plot configs
+        d = np.asarray(self.coord_class)
+        w_1, w_2, bias, epoch, lr = self._retrieve_values()
+        W = np.asarray([bias, w_1, w_2])
 
-        # draw the points
-        self._draw_classified_points(Y, X)
+        for _ in range(epoch):
+            p = Perceptron(W, d, lr)
+            p.train(X)
+            Y = p.activation(X)
+            W = p.W
 
-        # draw line
-        self._draw_line(W, bias)
+            self.ax.cla()       # clean the points on canvas
+            self._config_plot() # set plot configs
 
-        self.fig.canvas.draw()
+            self._set_fields(W)
+
+            # draw the points
+            self._draw_classified_points(Y, X)
+
+            # draw line
+            self._draw_line(W[1:], W[0])
+
+            self.fig.canvas.draw()
+            time.sleep(1)
+
 
     def _draw_classified_points(self, Y, X):
         for i in range(Y.shape[0]):
             if Y[i] == 1:
                 self.ax.plot(X[i][0], X[i][1], 'ro')
-            elif Y[i] == 0:
+            elif Y[i] == -1:
                 self.ax.plot(X[i][0], X[i][1], 'go')
 
     def _retrieve_values(self):
         w_1 = 0
         w_2 = 0
         bias = 0
+        epoch = 0
+        lr = 0
         
         try:
-            w_1 = float(self.w1.get())
-            w_2 = float(self.w2.get())
-            bias = float(self.bias.get())
+            w_1   = float(self.w1.get())
+            w_2   = float(self.w2.get())
+            bias  = float(self.bias.get())
+            epoch = int(self.epoch.get())
+            lr    = float(self.lr.get())
         except:
             pass
 
-        return w_1, w_2, bias
+        return w_1, w_2, bias, epoch, lr
 
     def _draw_line(self, W, bias):
         for i in np.linspace(-1, 1):
@@ -75,15 +92,29 @@ class Gui:
             y = (m * i) + b
 
             self.ax.plot(i, y, 'bo')
+
+    def _set_fields(self, W):
+        print(W)
+        self.w1.set(W[1].item())
+        self.w2.set(W[2].item())
+        self.bias.set(W[0].item())
     
     def onclick(self, event):
+        RIGHT_BUTTON = 3
+        LEFT_BUTTON  = 1
         try:
             x = event.xdata
             y = event.ydata
 
             if x is not None and y is not None:
+                if event.button == LEFT_BUTTON:
+                    self.ax.plot(x, y, 'go')
+                    self.coord_class.append(-1)
+                elif event.button == RIGHT_BUTTON:
+                    self.ax.plot(x, y, 'ro')
+                    self.coord_class.append(1)
+
                 self.coord.append([x, y])
-                self.ax.plot(x, y, 'ko')
         except:
             pass
         
@@ -107,28 +138,44 @@ class Gui:
         # set plot frame
         plotframe = ttk.Frame(mainframe, borderwidth=5,
                               relief="ridge", width=900, height=900)
-        plotframe.grid(column=0, row=1, columnspan=7, sticky=(N, S, E, W))
+        plotframe.grid(column=0, row=1, columnspan=7, rowspan=7,sticky=(N, S, E, W))
         
         # set entry for first weight
-        ttk.Label(mainframe, text="Primer peso").grid(column=0,
-                                                      row=0, sticky=E)
-        w1_entry = ttk.Entry(mainframe, width=6, textvariable=self.w1)
-        w1_entry.grid(column=1, row=0, sticky=W)
+        ttk.Label(mainframe, text="Primer peso").grid(column=9,
+                                                      row=1, sticky=E)
+        w1_entry = ttk.Entry(mainframe, width=20, textvariable=self.w1,
+                                state="disabled")
+        w1_entry.grid(column=10, row=1, sticky=W)
+        self.w1.set(self.weights[:, 1].item())
 
         # set entry for second weight
-        ttk.Label(mainframe, text="Segundo peso").grid(column=2,
-                                                       row=0, sticky=E)
-        w2_entry = ttk.Entry(mainframe, width=6, textvariable=self.w2)
-        w2_entry.grid(column=3, row=0, sticky=W)
+        ttk.Label(mainframe, text="Segundo peso").grid(column=9,
+                                                       row=2, sticky=E)
+        w2_entry = ttk.Entry(mainframe, width=20, textvariable=self.w2,
+                                state="disabled")
+        w2_entry.grid(column=10, row=2, sticky=W)
+        self.w2.set(self.weights[:, 2].item())
         
         # set entry for bias
-        ttk.Label(mainframe, text="Bias").grid(column=4, row=0, sticky=E)
-        bias_entry = ttk.Entry(mainframe, width=6, textvariable=self.bias)
-        bias_entry.grid(column=5, row=0, sticky=W)
+        ttk.Label(mainframe, text="Bias").grid(column=9, row=3, sticky=E)
+        bias_entry = ttk.Entry(mainframe, width=20, textvariable=self.bias,
+                                state="disabled")
+        bias_entry.grid(column=10, row=3, sticky=W)
+        self.bias.set(self.weights[:, 0].item())
+
+        # set entry for epoch
+        ttk.Label(mainframe, text="Épocas").grid(column=9, row=4, sticky=E)
+        bias_entry = ttk.Entry(mainframe, width=6, textvariable=self.epoch)
+        bias_entry.grid(column=10, row=4, sticky=W)
+
+        # set entry for learning rate (lr)
+        ttk.Label(mainframe, text="Learning Rate").grid(column=9, row=5, sticky=E)
+        bias_entry = ttk.Entry(mainframe, width=6, textvariable=self.lr)
+        bias_entry.grid(column=10, row=5, sticky=W)
 
         # set start button
         ttk.Button(mainframe, text="Clasificar", command=self.start).grid(
-            column=6, row=0, sticky=(E, W))
+            column=10, row=6, sticky=(E, W))
 
         canvas = FigureCanvasTkAgg(self.fig, master=plotframe)
         canvas.draw()
